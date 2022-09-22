07666273ff83a46bad6b4bde1da90d89fc788d46
Tyagi.Garvit <Tyagi.Garvit@INLPF2RX2LR>
2022-09-22 16:48:42 +0530

    Add project files.

---
+++ LeapYear/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the year");
+            int year= Convert.ToInt32(Console.ReadLine());
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+                Console.WriteLine("It is a leap year");
+            else
+                Console.WriteLine("It is not a leap year");
+
+
+        }
+    }
+}
